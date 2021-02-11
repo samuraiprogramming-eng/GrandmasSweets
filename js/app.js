d7de66986573b939b861f60d2c30abc6b79be5b1
samuraiprogramming-eng <sohaibfathy33@gmail.com>
2021-02-11 04:22:20 +0200

    GrandmasSweets

---
+++ js/app.js
@@ -0,0 +1,7 @@
+// ===================== button cart =============== //
+
+document.getElementById("cart-info").addEventListener("click", function() {
+  const cart = document.getElementById("cart");
+  cart.classList.toggle("show-cart");
+  console.log(cart);
+});
